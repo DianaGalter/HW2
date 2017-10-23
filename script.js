bbda6fe3fc3e9e6c7112c83df32d0c7d801a8822
Diana Bobrova <diana_uo@mail.ru>
2017-10-24 00:43:16 +0400

    described add/rm functions

--- script.js
+++ script.js
@@ -3,21 +3,69 @@ var wrapper = document.getElementsByClassName('wrapper')[0];
 var taskInput = document.getElementsByClassName('task-input')[0];
 var taskForm = document.getElementsByClassName('task-form')[0];
 var markAll = document.getElementsByClassName('mark-all')[0];
-var tasks = document.getElementsByClassName('task');
+var l = console.log;
 
 wrapper.onclick = function(e) {
     var target = e.target;
-}
+    if (target.classList.contains('delete')) {
+        removeTask(target);
+    };
+    if (target.classList.contains('deadline')) {
+        addDeadline(target);
+    };
+    if (target.classList.contains('mark-all')) {
+        allChecked();
+    };
+
+};
 taskForm.onsubmit = function(e) {
   e.preventDefault();
   addTask(taskInput.value);
-  console.log(taskInput.value);
+  markAll.classList.remove('invisible');
   return false;
-}
+};
 
 function addTask(task) {
-  ...
-}
+    var todoUl = document.getElementsByClassName('todo-list')[0];
+    var taskHTML = `<li class="todo-list__li">
+        <label class="task">
+            <input type="checkbox" class="mark">
+            <span class="task__text">` + task + `</span>
+        </label>
+        <a class="deadline" title="Set the deadline"></a>
+        <a class="delete" title="Delete the task"></a>
+    </li>`;
+    if (todoUl) {
+        todoUl.innerHTML += taskHTML;
+    } else {
+        var ul = document.createElement('ul');
+        ul.className = 'todo-list';
+        wrapper.appendChild(ul);
+        ul.innerHTML = taskHTML;
+    };
+    taskInput.value = "";
+};
+
 function removeTask(target) {
-  ...
+  target.parentNode.remove();
+};
+
+function addDeadline(target) {
+    var dataToday = document.createElement('input');
+    dataToday.type = "date";
+    dataToday.className = "dataToday";
+    dataToday.valueAsDate = new Date();
+    target.parentNode.insertBefore(dataToday, target);
+    target.remove();
 }
+
+function allChecked() {
+    var tasks = document.getElementsByClassName('task');
+    var marks = document.getElementsByClassName('mark');
+    var length = tasks.length;
+    for(var i = 0; i < length; i++) {
+        tasks[i].classList.toggle('cross-out');
+        marks[i].checked = !marks[i].checked;
+    };
+
+};
