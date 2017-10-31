b1fde7223538f23f2927eb472f2e03b3b867e153
Diana Bobrova <diana_uo@mail.ru>
2017-11-01 01:18:04 +0300

    added xhr

--- js/weather-request.js
+++ js/weather-request.js
@@ -1,30 +1,45 @@
 l = console.log;
 const API_KEY = 'afe096a642b6dde980dedc455ac4daf6';
 const CITY_LIST = document.getElementsByClassName('cube__town');
-const CITY_OF_CHOICE = document.getElementsByClassName('cube-forecast')[0];
+const CURRENT_CITY = document.getElementsByClassName('cube-forecast')[0];
+
+/*очень странное действие, но не знаю, как иначе избежать названий городов с пробелами*/
+var cityList = [];
 for (var i = 0; i < 5; i++) {
-	l(CITY_LIST[i].innerHTML);
+	cityList[i] = CITY_LIST[i].innerHTML.split(' ');
+	cityList[i].pop();
+	cityList[i].pop();
+	if (cityList[i].length > 1) {
+		cityList[i] = cityList[i].join(' ');
+	} else {
+		cityList[i] = cityList[i][0];
+	}
 };
 
-/*cityList.forEach(function(item, i) {
-	if(item.name === "Urbania") {
-		console.log(i)
-	};
-});*/
+
+/*узнаем погоду для известных нам городов*/
+for (var i = 0; i < 1; i++) {
+	l(cityList[i]);
+	request(cityList[i])
+}
 
 /*Повесим выбор города по клику на центральный виджет*/
-CITY_OF_CHOICE.onclick = function(e) {
+CURRENT_CITY.onclick = function(e) {
 	var target = e.target;
 };
 
+/*делаем запрос на сайт в соответствии с названием города*/
 function request (city) {
-	var api = 'http://api.openweathermap.org/data/2.5/forecast?q' + city + '&APPID=' + API_KEY;
+	var api = 'http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&APPID=' + API_KEY;
 	var xhr = new XMLHttpRequest();
 	xhr.open('GET', api);
 	xhr.send();
-	if (xhr.status != 200) {
-		l(error);
-	} else {
-		l(xhr.responseText);
-	}
-}
+	xhr.onreadystatechange = function() {
+	  if (xhr.readyState != 4) return;
+	  if (xhr.status != 200) {
+	    l(xhr.status + ': ' + xhr.statusText);
+	  } else {
+	    return xhr.responseText;
+	  }
+  };
+};
