bc1548b08ec204f24025116c136c4a8fd772464d
Diana Bobrova <diana_uo@mail.ru>
2017-10-31 23:58:00 +0300

    delete json

--- js/weather-request.js
+++ js/weather-request.js
@@ -2,6 +2,10 @@ l = console.log;
 const API_KEY = 'afe096a642b6dde980dedc455ac4daf6';
 const CITY_LIST = document.getElementsByClassName('cube__town');
 const CITY_OF_CHOICE = document.getElementsByClassName('cube-forecast')[0];
+for (var i = 0; i < 5; i++) {
+	l(CITY_LIST[i].innerHTML);
+};
+
 /*cityList.forEach(function(item, i) {
 	if(item.name === "Urbania") {
 		console.log(i)
@@ -12,3 +16,15 @@ const CITY_OF_CHOICE = document.getElementsByClassName('cube-forecast')[0];
 CITY_OF_CHOICE.onclick = function(e) {
 	var target = e.target;
 };
+
+function request (city) {
+	var api = 'http://api.openweathermap.org/data/2.5/forecast?q' + city + '&APPID=' + API_KEY;
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', api);
+	xhr.send();
+	if (xhr.status != 200) {
+		l(error);
+	} else {
+		l(xhr.responseText);
+	}
+}
